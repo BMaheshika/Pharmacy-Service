d5eb00aafd5897dcb9d7a2a007e7a345d2b4bdb6
BMaheshika <52059477+BMaheshika@users.noreply.github.com>
2020-05-05 21:45:49 +0530

    Modified js file

--- WebContent/Components/pharmacy.js
+++ WebContent/Components/pharmacy.js
@@ -11,6 +11,7 @@ $(document).on("click", "#btnSave", function(event) {
 	$("#alertSuccess").hide();
 	$("#alertError").text("");
 	$("#alertError").hide();
+	
 	// Form validation-------------------
 	var status = validatePharmacyForm();
 	if (status != true) {
@@ -21,19 +22,17 @@ $(document).on("click", "#btnSave", function(event) {
 	// If valid------------------------
 	$("#formPharmacy").submit();
 });
+
 // UPDATE==========================================
-$(document).on(
-		"click",
-		".btnUpdate",
-		function(event) {
+$(document).on("click",".btnUpdate",function(event) {
 			$("#hidRegIdSave").val(
-					$(this).closest("tr").find('#hidRegIdUpdate').val());
-			$("#phName").val($(this).closest("tr").find('td:eq(0)').text());
-			$("#phAddr").val($(this).closest("tr").find('td:eq(1)').text());
-			$("#phOwner").val($(this).closest("tr").find('td:eq(2)').text());
-			$("#contact").val($(this).closest("tr").find('td:eq(3)').text());
-			$("#regDate").val($(this).closest("tr").find('td:eq(4)').text());
-			$("#email").val($(this).closest("tr").find('td:eq(5)').text());
+			$(this).closest("tr").find('#hidRegIdUpdate').val());
+			$("#phName").val($(this).closest("tr").find('td:eq(2)').text());
+			$("#phAddr").val($(this).closest("tr").find('td:eq(3)').text());
+			$("#phOwner").val($(this).closest("tr").find('td:eq(4)').text());
+			$("#contact").val($(this).closest("tr").find('td:eq(5)').text());
+			$("#regDate").val($(this).closest("tr").find('td:eq(6)').text());
+			$("#email").val($(this).closest("tr").find('td:eq(7)').text());
 		});
 // CLIENTMODEL=========================================================================
 function validateItemForm() {
@@ -50,29 +49,10 @@ function validateItemForm() {
 	if ($("#phOwner").val().trim() == "") {
 		return "Insert Name of Owner.";
 	}
-	// is numerical value
-//	var tmpPrice = $("#itemPrice").val().trim();
-//	if (!$.isNumeric(tmpPrice)) {
-//		return "Insert a numerical value for Item Price.";
-//	}
-//	// convert to decimal price
-//	$("#itemPrice").val(parseFloat(tmpPrice).toFixed(2));
-	
-	
-	
-	
-	
-	//CONTACT
-//	var contact = /^[2-9]\d{2}[2-9]\d{2}\d{4}$/;
-//	  var digits = p.replace(/\D/g, "");
-//	  return contact.test(digits);
 	
 	if ($("#contact").val().trim() == "") {
 		return "Insert contact number.";
 	}
-	
-	
-	
 	
 	// DATE------------------------
 	if ($("#regDate").val().trim() == "") {
@@ -83,11 +63,7 @@ function validateItemForm() {
 		return "Insert Email Address.";
 	}
 	return true;
-	
-	//EMAIL
-//	var email = v.trim();
-//    return Validation.get('IsEmpty').test(email) || /^([a-z0-9,!\#\$%&'\*\+\/=\?\^_`\{\|\}~-]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z0-9,!\#\$%&'\*\+\/=\?\^_`\{\|\}~-]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*@([a-z0-9-]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z0-9-]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*\.(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]){2,})$/i.test(email);
-//	
+
 	
 	
 }
